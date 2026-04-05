febd6d9810fd6f973a09d429737306c6f9891153
DhimazRafif <hanafidhimaz@gmail.com>
2026-04-05 13:55:02 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103082430003/Program.cs
+++ tpmodul6_103082430003/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using tpmodul6_103082430003;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo film1 = new SayaTubeVideo("Tutorial Design By Contract - Dhimaz Rafif Hanafi");
+
+        film1.PrintVideoDetails();
+        film1.IncreasePlayCount(12);
+        film1.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_103082430003/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpmodul6_103082430003
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.playCount = 0;
+
+            Random random = new Random();
+            int generateId = random.Next(00000, 99999);
+            this.id = generateId;
+        }
+
+        public void IncreasePlayCount(int plusCount)
+        {
+            int hasil = playCount + plusCount;
+            playCount = hasil;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine($"Id Film : {id}");
+            Console.WriteLine($"Judul : {title}");
+            Console.WriteLine($"Play Count : {playCount}");
+        }
+    }
+}
